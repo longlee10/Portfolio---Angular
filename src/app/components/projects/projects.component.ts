cb064f9e3d5b9f018c01a8ed7108e77bdc49bcb8
longlee10 <longquan98@gmail.com>
2023-06-17 22:01:38 -0400

    Adding technologies to projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
+  intro =
+    'I have built multiple web applications using Angular, Bootstrap CSS, TypeScript and Javascript. You can find the source code on my Github. All projects are hosted on the cloud (Firebase or Heroku).';
   card = [
     {
       imgSrc: '/assets/img/Puzzle_store.png',
@@ -13,6 +15,7 @@ export class ProjectsComponent {
       href: 'https://puzzle-store-43bba.firebaseapp.com/',
       description:
         'In this project, I practice using Angular framework with Typescript, HTML, CSS (Bootstrap). Authentication and authorization features are implemented using Google Servive, and the website is hosted on Firebase.',
+      tech: ['angular', 'typescript', 'firebase', 'bootstrap'],
     },
     {
       imgSrc: '/assets/img/Aurora_research.png',
@@ -20,6 +23,15 @@ export class ProjectsComponent {
       href: 'https://aurora-research-company.herokuapp.com/landing',
       description:
         'Our team build this survey website with Angular, Bootstrap CSS and NodeJS. MongoDB is used to store data, and the site is hosted on Heroku.',
+      tech: ['angular', 'nodejs', 'mongodb'],
+    },
+    {
+      imgSrc: '/assets/img/Bug_smasher.png',
+      name: 'Bug Smasher',
+      href: 'http://studentweb.cencol.ca/hle81/BugSmasher/BugSmasher.html',
+      description:
+        'This simple application is created with HTML, CSS and Javascript. Users catch the bugs to score, after each time the bug is caught, it moves faster.',
+      tech: ['html', 'css', 'javascript'],
     },
   ];
 }
