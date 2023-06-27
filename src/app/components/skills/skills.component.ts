dbfc9e6496edea3ca99d9d946a54e6fe1502bcc0
longlee10 <longquan98@gmail.com>
2023-06-26 22:22:08 -0400

    Implementing download resume

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {
+  imgSrc: [];
+  private url: string = '/assets/json/skills.json';
 
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http.get(this.url).subscribe((json) => {
+      this.imgSrc = json['imgSrc'];
+    });
+  }
 }
