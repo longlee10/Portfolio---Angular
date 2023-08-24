951977059d3ddf4f929c7c6c65d79846b4700295
longlee10 <longquan98@gmail.com>
2023-08-24 19:09:14 -0400

    FIxing layout - nav bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  expanded = false;
+  toggle() {
+    this.expanded = !this.expanded;
+  }
+}
